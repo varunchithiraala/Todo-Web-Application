fc44dfe397bfdd3a408d7c456f39c925c798a652
varunchithiraala <140340608+varunchithiraala@users.noreply.github.com>
2024-09-25 14:33:00 +0530

    Todo Web Application Solution

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth/';
+const API_URL = 'https://todo-web-application-backend.onrender.com/api/auth/';
 
 const signup = (data) => {
   return axios.post(`${API_URL}signup`, data);
