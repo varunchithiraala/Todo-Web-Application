a650bc91f736b78c42644f0251702f03f5f15ca3
varunchithiraala <140340608+varunchithiraala@users.noreply.github.com>
2024-09-25 14:34:07 +0530

    Todo Web Application Solution

--- frontend/src/services/todoService.js
+++ frontend/src/services/todoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/todos/';
+const API_URL = 'https://todo-web-application-backend.onrender.com/api/todos/';
 
 const createTodo = (todo) => {
   return axios.post(API_URL, todo, {
