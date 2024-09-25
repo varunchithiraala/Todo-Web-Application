a2e902ccc1082cf9bfb506e8cfb8301192468b5e
varunchithiraala <140340608+varunchithiraala@users.noreply.github.com>
2024-09-25 14:33:40 +0530

    Todo Web Application Solution

--- frontend/src/services/profileService.js
+++ frontend/src/services/profileService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/profile/';
+const API_URL = 'https://todo-web-application-backend.onrender.com/api/profile/';
 
 const getProfile = async () => {
   return axios.get(API_URL, {
